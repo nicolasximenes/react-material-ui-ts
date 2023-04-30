501cfacd3c0c44b570d0a1b759e34f3d7d2473e6
Nicolas Ximenes <nicolasximenes2@gmail.com>
2023-04-30 16:14:53 -0300

    refactor: Arquivos desnecessários removidos

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cadastros
+
+Aplicação construída com ReactJS, Material UI 5 e TypeScript
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Iniciando a aplicação</h1>
+    </div>
+  );
+}
+
