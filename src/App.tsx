7bfff76bd79e7e24c692dbb248d7ff62e5738aa3
Nicolas Ximenes <nicolasximenes2@gmail.com>
2023-05-01 17:05:07 -0300

    feat: Adicionado temas e troca de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { ThemeProvider } from "@mui/material";
-import { LightTheme } from "./shared/themes";
+import { AppThemeProvider } from "./shared/contexts";
 
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <div className="App">
           <AppRoutes />
         </div>
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 }
 
