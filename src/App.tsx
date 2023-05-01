b3220edfc7f8e6f8cda6662f4be483c93c8103b2
Nicolas Ximenes <nicolasximenes2@gmail.com>
2023-05-01 14:24:51 -0300

    feat: Configuração de tema e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes";
 
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <AppRoutes />
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <div className="App">
+          <AppRoutes />
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
